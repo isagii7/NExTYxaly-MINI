e162e950d95fb530e7b7109166d9448be5df1a5f
isagii7 <dyyfirr@gmail.com>
2026-08-14 14:39:08 +0500

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ module.exports = {
      * @type {string}
      * @default "mongodb+srv://..."
      */
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://nextyi:nexxty@cluster0.aw6matf.mongodb.net/?appName=Cluster0',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://offarslan_db_user:arslanmd@cluster0.xrqkzwg.mongodb.net/?appName=Cluster0',
 
     // ═══════════════════════════════════════════════════════════════════════
     //  🤖 BOT IDENTITY
